74e9e02fbe3412bb2418c5984ddc982a3ed41181
re3turn <re3turn@gmail.com>
2025-04-09 23:22:39 +0900

    Migrate from Groovy DSL to Kotlin DSL for Gradle build scripts

---
+++ build.gradle.kts
@@ -0,0 +1,24 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+
+    dependencies {
+        classpath("com.android.tools.build:gradle:4.2.1")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.0")
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+tasks.register("clean", Delete::class) {
+    delete(layout.buildDirectory)
+}
---
+++ lib/build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    kotlin("jvm")
+    `maven-publish`
+    jacoco
+}
+
+dependencies {
+    implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.9.0")
+    testImplementation("junit:junit:4.13.2")
+}
+
+kotlin {
+    jvmToolchain(17)
+}
+
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+}
