ab87c227f7540ac8e3fd4cbd0b57607d6526abbb
re3turn <re3turn@gmail.com>
2025-04-11 23:39:07 +0900

    Update Kotlin version to 2.1.20 and modernize build configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,5 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 
-buildscript {
-    repositories {
-        google()
-        mavenCentral()
-    }
-
-    dependencies {
-        classpath("com.android.tools.build:gradle:4.2.1")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.0")
-    }
-}
 
 allprojects {
     repositories {
--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -1,21 +1,19 @@
 plugins {
-    kotlin("jvm")
+    kotlin("jvm") version "2.1.20"
     `maven-publish`
     jacoco
 }
 
 dependencies {
     implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.9.0")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:2.1.20")
     testImplementation("junit:junit:4.13.2")
 }
 
 kotlin {
     jvmToolchain(17)
-}
 
-tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-    kotlinOptions {
-        jvmTarget = "17"
+    compilerOptions {
+        jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
     }
 }
